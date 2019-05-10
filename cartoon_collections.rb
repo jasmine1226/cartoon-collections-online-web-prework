9ad6246aac24d56c5c1d7ff9769ee590b88fb40e
Jasmine Chen <jasmine1226@gmail.com>
2019-05-10 07:19:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,6 +10,8 @@ def summon_captain_planet(array)
   return array
 end
 
+summon_captain_planet(["carrot", "cucumber", "pepper"])
+
 def long_planeteer_calls# code an argument here
   # Your code here
 end
